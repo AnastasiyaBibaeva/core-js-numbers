87e48783e8f19985096ffcd790db039ebe82be86
AnastasiyaBibaeva <nasbibaeva@gmail.com>
2024-12-06 21:35:10 +0300

    update

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -222,10 +222,8 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(value, def) {
-  if (typeof parseFloat(value) === 'number') return value;
-  if (!typeof parseFloat(value).isNaN) return def;
-  return def;
+function toNumber(/* value, def */) {
+  throw new Error('Not implemented');
 }
 
 /**
